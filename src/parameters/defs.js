ae9f2bcd94f53f270a532affec6a9a842d52dd08
Carl Harris <ceharris@vt.edu>
2018-04-03 06:42:59 -0400

    move MIDI controller IDs to separate module

--- src/parameters/defs.js
+++ src/parameters/defs.js
@@ -83,10 +83,8 @@ import {
 } from "../parameters/names";
 
 class Parameter {
-  constructor(type, ctrlCoarse, ctrlFine, group, name, init) {
+  constructor(type, group, name, init) {
     this.type = type;
-    this.ctrlCoarse = ctrlCoarse;
-    this.ctrlFine = ctrlFine;
     this.group = group;
     this.name = name;
     this.init = init;
@@ -96,160 +94,155 @@ class Parameter {
     return state[this.group][this.name];
   }
 
-  toMidiControlMessage(state) {
-
-  }
 }
 
 class FlagParameter extends Parameter {
-  constructor(ctrlCoarse, ctrlFine, group, name, init) {
-    super(PARAM_TYPE_FLAG, ctrlCoarse, ctrlFine, group, name, 
-      init !== undefined ? init : false);
+  constructor(group, name, init) {
+    super(PARAM_TYPE_FLAG, group, name, init !== undefined ? init : false);
   }
 }
 
 class LevelParameter extends Parameter {
-  constructor (ctrlCoarse, ctrlFine, group, name, min, max, init) {
-    super(PARAM_TYPE_LEVEL, ctrlCoarse, ctrlFine, group, name, 
-      init !== undefined ? init : min);
+  constructor (group, name, min, max, init) {
+    super(PARAM_TYPE_LEVEL, group, name, init !== undefined ? init : min);
     this.min = min;
     this.max = max;
   }
 }
 
 class ChoiceParameter extends Parameter {
-  constructor (ctrlCoarse, ctrlFine, group, name, choices, init) {
-    super(PARAM_TYPE_CHOICE, ctrlCoarse, ctrlFine, group, name, 
-      init !== undefined ? init : choices[0]);
+  constructor (group, name, choices, init) {
+    super(PARAM_TYPE_CHOICE, group, name, 
+          init !== undefined ? init : choices[0]);
     this.choices = choices;
   }
 }
 
 const defs = {
   [POLYMOD_SOURCE_FILTER_ENV]: 
-    new LevelParameter(34, 98, PARAM_GROUP_POLYMOD, "sourceFilterEnv", 0, 255),
+    new LevelParameter(PARAM_GROUP_POLYMOD, "sourceFilterEnv", 0, 255),
   [POLYMOD_SOURCE_OSCILLATOR_B]:
-    new LevelParameter(35, 99, PARAM_GROUP_POLYMOD, "sourceOscillatorB", 0, 255),
+    new LevelParameter(PARAM_GROUP_POLYMOD, "sourceOscillatorB", 0, 255),
   [POLYMOD_DEST_FREQUENCY_A]: 
-    new FlagParameter(55, 0, PARAM_GROUP_POLYMOD, "destFrequencyA"),
+    new FlagParameter(PARAM_GROUP_POLYMOD, "destFrequencyA"),
   [POLYMOD_DEST_FILTER]: 
-    new FlagParameter(56, 0, PARAM_GROUP_POLYMOD, "destFilter"),
+    new FlagParameter(PARAM_GROUP_POLYMOD, "destFilter"),
   [LFO_FREQUENCY]: 
-    new LevelParameter(36, 100, PARAM_GROUP_LFO, "frequency", 0, 255),
+    new LevelParameter(PARAM_GROUP_LFO, "frequency", 0, 255),
   [LFO_RANGE]: 
-    new ChoiceParameter(58, 0, PARAM_GROUP_LFO, "range", [ "low", "high"]),
+    new ChoiceParameter(PARAM_GROUP_LFO, "range", [ "low", "high"]),
   [LFO_SHAPE]: 
-    new ChoiceParameter(57, 0, PARAM_GROUP_LFO, "shape", 
+    new ChoiceParameter(PARAM_GROUP_LFO, "shape", 
         [ "triangle", "sine", "sawtooth", "pulse", "random", "noise" ]),
   [LFO_DEPTH]: 
-    new LevelParameter(37, 101, PARAM_GROUP_LFO, "depth", 0, 255),
+    new LevelParameter(PARAM_GROUP_LFO, "depth", 0, 255),
   [LFO_DELAY]: 
-    new LevelParameter(41, 105, PARAM_GROUP_LFO, "delay", 0, 255),
+    new LevelParameter(PARAM_GROUP_LFO, "delay", 0, 255),
   [LFO_DEST_TARGET]: 
-    new ChoiceParameter(59, 0, PARAM_GROUP_LFO, "destinationTarget",
+    new ChoiceParameter(PARAM_GROUP_LFO, "destinationTarget",
         [ "a", "b", "ab"]),
   [LFO_DEST_FREQUENCY]: 
-    new FlagParameter(59, 0, PARAM_GROUP_LFO, "destinationFrequency"),
+    new FlagParameter(PARAM_GROUP_LFO, "destinationFrequency"),
   [LFO_DEST_PULSE_WIDTH]: 
-    new FlagParameter(59, 0, PARAM_GROUP_LFO, "destinationPulseWidth"),
+    new FlagParameter(PARAM_GROUP_LFO, "destinationPulseWidth"),
   [LFO_DEST_FILTER]: 
-    new FlagParameter(59, 0, PARAM_GROUP_LFO, "destinationFilter"),
+    new FlagParameter(PARAM_GROUP_LFO, "destinationFilter"),
   [VIBRATO_FREQUENCY]: 
-    new LevelParameter(42, 106, PARAM_GROUP_VIBRATO, "frequency", 0, 255),
+    new LevelParameter(PARAM_GROUP_VIBRATO, "frequency", 0, 255),
   [VIBRATO_DEPTH]: 
-    new LevelParameter(43, 107, PARAM_GROUP_VIBRATO, "depth", 0, 255),
+    new LevelParameter(PARAM_GROUP_VIBRATO, "depth", 0, 255),
   [UNISON_TRACK]: 
-    new FlagParameter(65, 0, PARAM_GROUP_UNISON, "track"),
+    new FlagParameter(PARAM_GROUP_UNISON, "track"),
   [UNISON_DETUNE]: 
-    new LevelParameter(44, 108, PARAM_GROUP_UNISON, "detune", 0, 255),
+    new LevelParameter(PARAM_GROUP_UNISON, "detune", 0, 255),
   [OSCILLATOR_A_FREQUENCY]: 
-    new LevelParameter(16, 80, PARAM_GROUP_OSCILLATOR_A, "frequency", 0, 255),
+    new LevelParameter(PARAM_GROUP_OSCILLATOR_A, "frequency", 0, 255),
   [OSCILLATOR_A_SYNC]: 
-    new FlagParameter(54, 0, PARAM_GROUP_OSCILLATOR_A, "sync"),
+    new FlagParameter(PARAM_GROUP_OSCILLATOR_A, "sync"),
   [OSCILLATOR_A_SHAPE_SAWTOOTH]: 
-    new FlagParameter(48, 0, PARAM_GROUP_OSCILLATOR_A, "shapeSawtooth"),
+    new FlagParameter(PARAM_GROUP_OSCILLATOR_A, "shapeSawtooth"),
   [OSCILLATOR_A_SHAPE_TRIANGLE]:
-    new FlagParameter(49, 0, PARAM_GROUP_OSCILLATOR_A, "shapeTriangle"),  
+    new FlagParameter(PARAM_GROUP_OSCILLATOR_A, "shapeTriangle"),  
   [OSCILLATOR_A_SHAPE_PULSE]:
-    new FlagParameter(50, 0, PARAM_GROUP_OSCILLATOR_A, "shapePulse"),
+    new FlagParameter(PARAM_GROUP_OSCILLATOR_A, "shapePulse"),
   [OSCILLATOR_A_PULSE_WIDTH]: 
-    new LevelParameter(18, 82, PARAM_GROUP_OSCILLATOR_A, "pulseWidth", 0, 255),
+    new LevelParameter(PARAM_GROUP_OSCILLATOR_A, "pulseWidth", 0, 255),
   [OSCILLATOR_B_FREQUENCY]: 
-    new LevelParameter(19, 83, PARAM_GROUP_OSCILLATOR_B, "frequency", 0, 255),
+    new LevelParameter(PARAM_GROUP_OSCILLATOR_B, "frequency", 0, 255),
   [OSCILLATOR_B_FINE]: 
-    new LevelParameter(22, 86, PARAM_GROUP_OSCILLATOR_B, "fine", 0, 255),
+    new LevelParameter(PARAM_GROUP_OSCILLATOR_B, "fine", 0, 255),
   [OSCILLATOR_B_SHAPE_SAWTOOTH]: 
-    new FlagParameter(51, 0, PARAM_GROUP_OSCILLATOR_B, "shapeSawtooth"),
+    new FlagParameter(PARAM_GROUP_OSCILLATOR_B, "shapeSawtooth"),
   [OSCILLATOR_B_SHAPE_TRIANGLE]:
-    new FlagParameter(52, 0, PARAM_GROUP_OSCILLATOR_B, "shapeTriangle"),  
+    new FlagParameter(PARAM_GROUP_OSCILLATOR_B, "shapeTriangle"),  
   [OSCILLATOR_B_SHAPE_PULSE]:
-    new FlagParameter(53, 0, PARAM_GROUP_OSCILLATOR_B, "shapePulse"),
+    new FlagParameter(PARAM_GROUP_OSCILLATOR_B, "shapePulse"),
   [OSCILLATOR_B_PULSE_WIDTH]: 
-    new LevelParameter(21, 85, PARAM_GROUP_OSCILLATOR_B, "pulseWidth", 0, 255),
+    new LevelParameter(PARAM_GROUP_OSCILLATOR_B, "pulseWidth", 0, 255),
   [MIXER_OSCILLATOR_A_LEVEL]: 
-    new LevelParameter(17, 81, PARAM_GROUP_MIXER, "oscillatorALevel", 0, 255),
+    new LevelParameter(PARAM_GROUP_MIXER, "oscillatorALevel", 0, 255),
   [MIXER_OSCILLATOR_B_LEVEL]:     
-    new LevelParameter(20, 84, PARAM_GROUP_MIXER, "oscillatorBLevel", 0, 255),
+    new LevelParameter(PARAM_GROUP_MIXER, "oscillatorBLevel", 0, 255),
   [FILTER_CUTOFF]:
-    new LevelParameter(23, 87, PARAM_GROUP_FILTER, "cutoff", 0, 255),
+    new LevelParameter(PARAM_GROUP_FILTER, "cutoff", 0, 255),
   [FILTER_RESONANCE]:
-    new LevelParameter(24, 88, PARAM_GROUP_FILTER, "resonance", 0, 255),
+    new LevelParameter(PARAM_GROUP_FILTER, "resonance", 0, 255),
   [FILTER_ENVELOPE_AMOUNT]:
-    new LevelParameter(25, 89, PARAM_GROUP_FILTER, "envelopeAmount", 0, 255),
+    new LevelParameter(PARAM_GROUP_FILTER, "envelopeAmount", 0, 255),
   [FILTER_KEYBOARD_TRACK]:
-    new ChoiceParameter(60, 0, PARAM_GROUP_FILTER, "keyboardTrack", 
+    new ChoiceParameter(PARAM_GROUP_FILTER, "keyboardTrack", 
         [ "off", "1/2", "full"], "full"),
   [FILTER_ENVELOPE_CURVE]:
-    new ChoiceParameter(61, 0, PARAM_GROUP_FILTER, "curve", 
+    new ChoiceParameter(PARAM_GROUP_FILTER, "curve", 
         [ "linear", "exponential"]),
   [FILTER_ENVELOPE_RATE]:
-    new ChoiceParameter(62, 0, PARAM_GROUP_FILTER, "rate", 
+    new ChoiceParameter(PARAM_GROUP_FILTER, "rate", 
         [ "slow", "fast"]),
   [FILTER_ENVELOPE_ATTACK]:
-    new LevelParameter(29, 93, PARAM_GROUP_FILTER, "attack", 0, 255),
+    new LevelParameter(PARAM_GROUP_FILTER, "attack", 0, 255),
   [FILTER_ENVELOPE_DECAY]:
-    new LevelParameter(28, 92, PARAM_GROUP_FILTER, "decay", 0, 255),
+    new LevelParameter(PARAM_GROUP_FILTER, "decay", 0, 255),
   [FILTER_ENVELOPE_SUSTAIN]:
-    new LevelParameter(27, 91, PARAM_GROUP_FILTER, "sustain", 0, 255),
+    new LevelParameter(PARAM_GROUP_FILTER, "sustain", 0, 255),
   [FILTER_ENVELOPE_RELEASE]:
-    new LevelParameter(26, 90, PARAM_GROUP_FILTER, "release", 0, 255),
+    new LevelParameter(PARAM_GROUP_FILTER, "release", 0, 255),
   [FILTER_ENVELOPE_VELOCITY]:
-    new LevelParameter(40, 104, PARAM_GROUP_FILTER, "velocity", 0, 255),
+    new LevelParameter(PARAM_GROUP_FILTER, "velocity", 0, 255),
   [AMPLIFIER_ENVELOPE_CURVE]:
-    new ChoiceParameter(63, 0, PARAM_GROUP_AMPLIFIER, "curve", 
+    new ChoiceParameter(PARAM_GROUP_AMPLIFIER, "curve", 
         [ "linear", "exponential"]),
   [AMPLIFIER_ENVELOPE_RATE]:
-    new ChoiceParameter(64, 0, PARAM_GROUP_AMPLIFIER, "rate", 
+    new ChoiceParameter(PARAM_GROUP_AMPLIFIER, "rate", 
         [ "slow", "fast"]),
   [AMPLIFIER_ENVELOPE_ATTACK]:
-    new LevelParameter(33, 97, PARAM_GROUP_AMPLIFIER, "attack", 0, 255),
+    new LevelParameter(PARAM_GROUP_AMPLIFIER, "attack", 0, 255),
   [AMPLIFIER_ENVELOPE_DECAY]:
-    new LevelParameter(32, 96, PARAM_GROUP_AMPLIFIER, "decay", 0, 255),
+    new LevelParameter(PARAM_GROUP_AMPLIFIER, "decay", 0, 255),
   [AMPLIFIER_ENVELOPE_SUSTAIN]:
-    new LevelParameter(31, 95, PARAM_GROUP_AMPLIFIER, "sustain", 0, 255),
+    new LevelParameter(PARAM_GROUP_AMPLIFIER, "sustain", 0, 255),
   [AMPLIFIER_ENVELOPE_RELEASE]:
-    new LevelParameter(30, 94, PARAM_GROUP_AMPLIFIER, "release", 0, 255),
+    new LevelParameter(PARAM_GROUP_AMPLIFIER, "release", 0, 255),
   [AMPLIFIER_ENVELOPE_VELOCITY]:
-    new LevelParameter(39, 103, PARAM_GROUP_AMPLIFIER, "velocity", 0, 255),
+    new LevelParameter(PARAM_GROUP_AMPLIFIER, "velocity", 0, 255),
   [PERFORMANCE_BEND_WHEEL_RANGE]: 
-    new ChoiceParameter(64, 0, PARAM_GROUP_PERFORMANCE, "bendWheelRange",
+    new ChoiceParameter(PARAM_GROUP_PERFORMANCE, "bendWheelRange",
         [ "2nd", "3rd", "5th", "octave" ]),
   [PERFORMANCE_BEND_WHEEL_TARGET]: 
-    new ChoiceParameter(68, 0, PARAM_GROUP_PERFORMANCE, "bendWheelTarget",
+    new ChoiceParameter(PARAM_GROUP_PERFORMANCE, "bendWheelTarget",
         [ "off", "pitch", "filter", "volume" ]),
   [PERFORMANCE_MOD_WHEEL_RANGE]:
-    new ChoiceParameter(69, 0, PARAM_GROUP_PERFORMANCE, "modWheelRange",
+    new ChoiceParameter(PARAM_GROUP_PERFORMANCE, "modWheelRange",
         [ "min", "low", "high", "max" ]),
   [PERFORMANCE_MOD_WHEEL_TARGET]: 
-    new ChoiceParameter(71, 0, PARAM_GROUP_PERFORMANCE, "modWheelTarget",
+    new ChoiceParameter(PARAM_GROUP_PERFORMANCE, "modWheelTarget",
         [ "lfo", "vibrato" ]),
   [PERFORMANCE_KEYBOARD_KEY_ASSIGN]: 
-    new ChoiceParameter(66, 0, PARAM_GROUP_PERFORMANCE, "keyboardKeyAssign",
+    new ChoiceParameter(PARAM_GROUP_PERFORMANCE, "keyboardKeyAssign",
         [ "last", "low", "high" ]),
   [PERFORMANCE_KEYBOARD_GLIDE]: 
-    new LevelParameter(38, 102, PARAM_GROUP_PERFORMANCE, "keyboardGlide", 0, 255),
+    new LevelParameter(PARAM_GROUP_PERFORMANCE, "keyboardGlide", 0, 255),
   [GLOBAL_FREQUENCY_STEP]: 
-    new ChoiceParameter(70, 0, PARAM_GROUP_GLOBAL, "frequencyStep",
+    new ChoiceParameter(PARAM_GROUP_GLOBAL, "frequencyStep",
         [ "free", "semitone", "octave" ], "semitone"),
 };
 
